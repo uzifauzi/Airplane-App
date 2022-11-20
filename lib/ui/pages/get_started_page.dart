b88b5eca9c0696e94e44300d3097e0add936d676
uzifauzi <nurfauzi.dev@gmail.com>
2022-11-20 23:56:32 +0700

    feat: add splash page and get started page

---
+++ lib/ui/pages/get_started_page.dart
@@ -0,0 +1,80 @@
+import 'package:airplane_app/shared/theme.dart';
+import 'package:airplane_app/shared/theme.dart';
+import 'package:airplane_app/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+import '../../shared/theme.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      'assets/image_get_started.png',
+                    ))),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Fly Like A Bird',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 32,
+                    fontWeight: bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Explore new world with us and let\nyour self get an amazing experiences',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: light,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  width: 220,
+                  height: 55,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: kPrimaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17.0),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Get Started',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: medium,
+                        ),
+                      )),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,40 @@
+import 'package:airplane_app/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: EdgeInsets.only(
+                bottom: 50,
+              ),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                'assets/icon_plane.png',
+              ))),
+            ),
+            Text(
+              'AIRPLANE',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: medium,
+                letterSpacing: 10,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
