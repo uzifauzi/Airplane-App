6039eed04e709a0d07b3bf58d36d462f1c209d9d
uzifauzi <nurfauzi.dev@gmail.com>
2022-11-22 09:33:05 +0700

    refactor: add 2 formfield

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -1,7 +1,10 @@
+import 'dart:html';
+
 import 'package:airplane_app/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 import '../../shared/theme.dart';
+import 'dart:html';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
@@ -15,8 +18,140 @@ class SignUpPage extends StatelessWidget {
         padding: EdgeInsets.symmetric(
           horizontal: defaultMargin,
         ),
-        children: [Title()],
+        children: [Title(), InputSection()],
       )),
+    );
+  }
+}
+
+class InputSection extends StatelessWidget {
+  const InputSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 30.0,
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 24,
+        vertical: 30,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(defaultRadius),
+        color: kWhiteColor,
+      ),
+      child: Column(
+        children: [
+          InputForm(
+            label: 'Full name',
+            hintText: 'Your full name',
+          ),
+          EmailForm(
+            label: 'Email name',
+            hintText: 'Your email name',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InputForm extends StatelessWidget {
+  final String label;
+  final String hintText;
+
+  const InputForm({
+    Key? key,
+    required this.label,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: 20,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          TextFormField(
+            cursorColor: kBlackColor,
+            decoration: InputDecoration(
+                hintText: hintText,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(
+                    defaultRadius,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ))),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class EmailForm extends StatelessWidget {
+  final String label;
+  final String hintText;
+
+  const EmailForm({
+    Key? key,
+    required this.label,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: 20,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: kBlackColor,
+            decoration: InputDecoration(
+                hintText: hintText,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(
+                    defaultRadius,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ))),
+          )
+        ],
+      ),
     );
   }
 }
