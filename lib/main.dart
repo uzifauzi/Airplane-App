bbf0e862c7b398e88dad7821aa4ea2986fd3668e
uzifauzi <nurfauzi.dev@gmail.com>
2022-12-04 20:31:33 +0700

    feat: setting firebase in app

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ import 'package:airplane_app/ui/pages/get_started_page.dart';
 import 'package:airplane_app/ui/pages/main_page.dart';
 import 'package:airplane_app/ui/pages/sign_up_page.dart';
 import 'package:airplane_app/ui/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp;
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
